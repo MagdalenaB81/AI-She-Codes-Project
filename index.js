1bda4de29b6a60299ec05990da24dea753965336
MagdalenaB81 <mbialek81@gmail.com>
2025-01-26 22:31:29 +0100

    added a new font

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "93dt143fob6ba6cd0da98437ffd064b8";
   let context =
-    "You are a poem expert and you love to write short poems, your mission is to generate a four line poem. Make sure to follow user instrctions";
-  let prompt = `User instructions are: Generate a poem about ${instructionInput.value}`;
+    "You are a poem expert and you love to write short poems, your mission is to generate a four line poem. Each verse should be in a new line. Make sure to follow user instrctions";
+  let prompt = `User instructions are: Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
